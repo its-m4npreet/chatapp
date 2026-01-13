b247e79d5fa498a5b1d9b40d1fd6aeb37c90e7a7
its-m4npreet <m4npreet006@gmail.com>
2026-01-13 19:35:56 +0530

    feat: remove unused builds configuration from Vercel and format Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase limit to 1MB (default is 500KB)
+  },
 })
